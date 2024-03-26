a965e30fb3fce384c1c6be9bb175a5da8c7b374c
Murilo <mbalves1@outlook.com>
2024-03-26 11:26:27 -0300

    [fix]: entendendo toBe toEqual

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -33,7 +33,8 @@ describe('numbers cleanNumbers', () => {
 
     const cleanedNumbers = cleanNumbers(numberValues)
 
-    expect(cleanedNumbers[0]).toBeTypeOf('number')
+    // expect(cleanedNumbers[0]).toBeTypeOf('number')
+    expect(cleanedNumbers).toEqual([1, 2])
   })
 
   it('should throw an error if an array with at least one empty string is provided', () => {
